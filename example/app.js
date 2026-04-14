93b2e807e89d903befc92aba2d0750e39c617dfd
hugoqdesh <gitjohan5@gmail.com>
2026-04-14 19:52:59 +0300

    router + small changes

--- example/app.js
+++ example/app.js
@@ -2,39 +2,39 @@ const count = new Reactive(0);
 count.subscribe(() => rerender(app()));
 
 function Counter() {
-  return createElement(
-    "div",
-    {},
-    createElement("p", {}, `Count: ${count.value}`),
-    createElement("button", { onClick: () => count.value++ }, "Increment"),
-  );
+	return createElement(
+		"div",
+		{},
+		createElement("p", {}, `Count: ${count.value}`),
+		createElement("button", { onClick: () => count.value++ }, "Increment"),
+	);
 }
 
 function About() {
-  return createElement(
-    "div",
-    {},
-    createElement("h1", {}, "About Page"),
-    createElement(
-      "button",
-      { onClick: () => Router.navigate(baseUrl + "/") },
-      "Go Home",
-    ),
-  );
+	return createElement(
+		"div",
+		{},
+		createElement("h1", {}, "About Page"),
+		createElement(
+			"button",
+			{ onClick: () => Router.navigate(baseUrl + "/") },
+			"Go Home",
+		),
+	);
 }
 
 function app() {
-  return createElement(
-    "div",
-    {},
-    createElement("h1", {}, "My Framework 🚀"),
-    createElement(Counter),
-    createElement(
-      "button",
-      { onClick: () => Router.navigate(baseUrl + "/about") },
-      "Go to About",
-    ),
-  );
+	return createElement(
+		"div",
+		{},
+		createElement("h1", {}, "My Framework 🚀"),
+		createElement(Counter),
+		createElement(
+			"button",
+			{ onClick: () => Router.navigate(baseUrl + "/about") },
+			"Go to About",
+		),
+	);
 }
 
 const baseUrl = "/example";
@@ -42,6 +42,6 @@ Router.add(baseUrl + "/", app);
 Router.add(baseUrl + "/about", About);
 
 window.onload = () => {
-  const root = document.getElementById("root");
-  Router.init(root);
+	const root = document.getElementById("root");
+	Router.init(root);
 };
