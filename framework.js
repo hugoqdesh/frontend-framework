86bcbf56d6920c02dd03e7272ec42f09a5875a72
hugoqdesh <gitjohan5@gmail.com>
2026-04-14 08:56:32 +0300

    Rerendering

--- framework.js
+++ framework.js
@@ -1,57 +1,77 @@
 class Reactive {
-  constructor(value) {
-    this._value = value;
-    this.subscribers = new Set();
-  }
+	constructor(value) {
+		this._value = value;
+		this.subscribers = new Set();
+	}
 
-  get value() {
-    return this._value;
-  }
+	get value() {
+		return this._value;
+	}
 
-  set value(newValue) {
-    this._value = newValue;
-    this.subscribers.forEach((fn) => fn());
-  }
+	set value(newValue) {
+		this._value = newValue;
+		this.subscribers.forEach((fn) => fn());
+	}
 
-  subscribe(fn) {
-    this.subscribers.add(fn);
-  }
+	subscribe(fn) {
+		this.subscribers.add(fn);
+	}
 }
 
 class Component {
-  constructor(props) {
-    this.props = props;
-  }
+	constructor(props) {
+		this.props = props;
+	}
 
-  render() {
-    return createElement("div", {}, "Default Component");
-  }
+	render() {
+		return createElement("div", {}, "Default Component");
+	}
 }
 
 function createElement(tag, props, ...children) {
-  return { tag, props, children };
+	return { tag, props, children };
 }
 
 function renderElement(node) {
-  if (typeof node === "string") return document.createTextNode(node);
+	if (typeof node === "string") return document.createTextNode(node);
 
-  const el = document.createElement(node.tag);
+	const el = document.createElement(node.tag);
 
-  if (node.props) {
-    Object.entries(node.props).forEach(([key, value]) => {
-      if (key.startsWith("on")) {
-        const eventName = key.toLowerCase().substring(2);
-        el.addEventListner(eventName, value);
-      } else {
-        el.setAttribute(key, value);
-      }
-    });
-  }
+	if (node.props) {
+		Object.entries(node.props).forEach(([key, value]) => {
+			if (key.startsWith("on")) {
+				const eventName = key.toLowerCase().substring(2);
+				el.addEventListener(eventName, value);
+			} else {
+				el.setAttribute(key, value);
+			}
+		});
+	}
 
-  node.children.map(renderElement).forEach((child) => el.appendChild(child));
-  return el;
+	node.children.map(renderElement).forEach((child) => el.appendChild(child));
+	return el;
 }
 
 function mount(vnode, container) {
-  container.appendChild(renderElement(vnode));
+	container.appendChild(renderElement(vnode));
 }
+
+const state = new Reactive(0);
+
+function App() {
+	return createElement(
+		"div",
+		{},
+		`Count: ${state.value}`,
+		createElement("button", { onclick: () => state.value++ }, "+"),
+	);
+}
+
+function renderApp() {
+	const root = document.getElementById("root");
+	root.innerHTML = "";
+	mount(App(), root);
+}
+
+state.subscribe(renderApp);
+renderApp();
